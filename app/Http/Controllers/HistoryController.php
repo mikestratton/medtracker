3309b37936caf6ecbab4c7eaad6349b4d9fa1f4f
mikestratton <mike@mikestratton.net>
2025-03-26 21:35:10 -0400

    history controller descending

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -11,34 +11,10 @@ class HistoryController extends Controller
     public function index()
     {
         $user_id = (int) Auth::id();
-        $events = Event::where('user_id', $user_id)->get();
+        $events = Event::where('user_id', $user_id)
+            ->orderBy('date', 'desc')
+            ->get();
 
         return view('history.index', compact('events'));
-    }
-
-    public function update(Request $request, Event $event)
-    {
-        $user_id = Auth::id();
-//        dd($request->all());
-        if ((int) $request->user_id !== $user_id) {
-            return 'wrong user id';
-            abort(403);
-        }
-
-        $validatedData = $request->validate([
-            'user_id' => 'integer',
-            'has_taken_medication' => 'integer',
-            'note' => 'string',
-            'date' => 'date',
-            'time' => 'string',
-        ]);
-
-        dd($validatedData);
-
-        $event->update($validatedData);
-
-//        $event->update($request->all());
-
-        return redirect()->route('history.index');
     }
 }
